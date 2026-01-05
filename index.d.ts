b6db49e4192db696194afd4e9d677ddc2102a8ce
104259 <104259@RD-03250003>
2026-01-05 09:21:27 +0800

    ui

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,12 @@ export const Button: any;
 export const Button2: any;
 export const DataGridApi: any;
 export const apitransform: any;
+export const apitransformDrop: any;
+export const Mytab: any;
+export const myModal: any;
+export const DynamicForm: any;
+
+
+
+
+
