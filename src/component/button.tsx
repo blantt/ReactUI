8f7e59bee692b73e3352708df368c6cb490768a8
104259 <104259@RD-03250003>
2026-01-17 02:59:39 +0800

    modal

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -9,8 +9,7 @@ export const Button = ({ label, onClick }: ButtonProps) => {
     return (
         <button
             className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
-            onClick={onClick}
-        >
+            onClick={onClick}  >
             {label}
         </button>
     );
