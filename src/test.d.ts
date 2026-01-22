a5412ff780f8850600422403f96fa1a32224b3aa
104259 <104259@RD-03250003>
2026-01-22 11:09:31 +0800

    ui

--- src/test.d.ts
+++ src/test.d.ts
@@ -1,18 +1,21 @@
-export const Button: any;
-export const Button2: any;
+// export const Button: any;
+// export const Button2: any;
 //export const DataGridApi: any;
  
+export { default as Button ,Button2} from './component/button';
 export { default as DataGridApi } from './component/myDataGrid';// 這樣會同時匯出組件與型別
- 
+export { default as DynamicForm} from './component/DynamicForm';
+export { default as SmartModal} from './component/SmartModal';
+
 export const apitransform: any;
 export const apitransformDrop: any;
 export const Mytab: any;
 export const Loading: any;
-export const DynamicForm: any;
+//export const DynamicForm: any;
 export const MyDropDown: any;
 export const MyDropGrid: any;
 export const AppTitle: any;
 export const Modal: any;
 export const LoadingInline: any;
 export const TextInput: any;
-export const SmartModal: any;
+//export const SmartModal: any;
