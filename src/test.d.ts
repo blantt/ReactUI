5cdb4d6cc39d4214c189b741a81367e5879dbc2e
104259 <104259@RD-03250003>
2026-01-22 10:58:59 +0800

    ui

---
+++ src/test.d.ts
@@ -0,0 +1,18 @@
+export const Button: any;
+export const Button2: any;
+//export const DataGridApi: any;
+ 
+export * from './component/myDataGrid';// 這樣會同時匯出組件與型別
+ 
+export const apitransform: any;
+export const apitransformDrop: any;
+export const Mytab: any;
+export const Loading: any;
+export const DynamicForm: any;
+export const MyDropDown: any;
+export const MyDropGrid: any;
+export const AppTitle: any;
+export const Modal: any;
+export const LoadingInline: any;
+export const TextInput: any;
+export const SmartModal: any;
