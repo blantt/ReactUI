03c786a902ed76dcfb40a6a0302828fca06f83aa
104259 <104259@RD-03250003>
2026-01-23 19:42:10 +0800

    ui

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   ],
   build: {
     lib: {
-      entry: 'src/index.ts', // 您的元件入口
+      entry: 'src/index.js', // 您的元件入口
       name: 'MyComponentLib',
      // fileName: (format) => `index.js`, // 固定檔名為 index.js
       formats: ['es', 'cjs']
