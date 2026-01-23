3fce8aa53f6b505c9e4679df9b979df7a1be9bb0
104259 <104259@RD-03250003>
2026-01-23 19:38:54 +0800

    ui

--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,17 @@ import dts from 'vite-plugin-dts'  //類別庫自動補完功能(但目前試沒
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Library Mode之後
+// Library Mode之前
 export default defineConfig({
   plugins: [react(),
-       dts({
-        insertTypesEntry: true, // 自動在 package.json 產生正確的入口宣告
-        rollupTypes: true,      // 將所有的 .d.ts 檔案強行合併成一個 index.d.ts
-})
+       dts({ insertTypesEntry: true }) // 這會自動幫你產生 .d.ts 並關聯到 package.json
   ],
   build: {
-    // lib: {
-    //   entry: 'src/index.js', // 您的元件入口
-    //   name: 'MyComponentLib',
-    //   formats: ['es', 'cjs']
-    // },
     lib: {
-      // 這是你元件庫的入口檔案
-      entry: 'src/index.ts', 
-      // 別人 import 時顯示的名稱 (例如在 UMD 模式下)
+      entry: 'src/index.ts', // 您的元件入口
       name: 'MyComponentLib',
-      // 輸出的檔案名稱
-      fileName: 'index',
-      // 你想要的格式：es 是現代格式 (.mjs)，umd 是相容格式 (.cjs)
-      formats: ['es', 'umd'] 
+     // fileName: (format) => `index.js`, // 固定檔名為 index.js
+      formats: ['es', 'cjs']
     },
     rollupOptions: {
       // 確保不要把 react 打包進去
