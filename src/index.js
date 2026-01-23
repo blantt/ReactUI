054ad7c192a841f786774d2ac8c8aece81949999
104259 <104259@RD-03250003>
2026-01-23 11:58:20 +0800

    ui

--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
-export { default as Button,Button2 } from './component/button';
-export { transformToFormField as apitransform } from './component/myDataGrid.tsx';
-export { default as DataGridApi } from './component/myDataGrid.tsx';
-export { default as Mytab } from './component/myTab.tsx';
-export { default as CardGrid } from './component/CardGrid.tsx'; 
-export { default as AppTitle } from './component/header.tsx'; 
-export { default as MyDropDown } from './component/myDropDown.tsx';  
-export { default as MyDropGrid ,  transformToFormField as apitransformDrop } from './component/myDropGrid.tsx';  
-export { default as Loading } from './component/myload.tsx'; 
-export { default as Modal } from './component/myModal.tsx'; 
-export { default as DynamicForm } from './component/DynamicForm.tsx';  
+// export { default as Button,Button2 } from './component/button';
+// export { transformToFormField as apitransform } from './component/myDataGrid.tsx';
+// export { default as DataGridApi } from './component/myDataGrid.tsx';
+// export { default as Mytab } from './component/myTab.tsx';
+// export { default as CardGrid } from './component/CardGrid.tsx'; 
+// export { default as AppTitle } from './component/header.tsx'; 
+// export { default as MyDropDown } from './component/myDropDown.tsx';  
+// export { default as MyDropGrid ,  transformToFormField as apitransformDrop } from './component/myDropGrid.tsx';  
+// export { default as Loading } from './component/myload.tsx'; 
+// export { default as Modal } from './component/myModal.tsx'; 
+// export { default as DynamicForm } from './component/DynamicForm.tsx';  
+// export { LoadingInline } from './component/myload'; 
+// export { TextInput } from './component/simpleUI.tsx'; 
+// export {  default as SmartModal } from './component/simpleUI.tsx'; 
+//  export { default as SmartModal_test,Modal_test} from './component/SmartModal_test'; 
+
+
+export { default as Button ,Button2} from './component/button';
+export { default as DataGridApi } from './component/myDataGrid';// 這樣會同時匯出組件與型別
+export { default as DynamicForm} from './component/DynamicForm';
+export { default as SmartModal} from './component/SmartModal';
+ export { default as SmartModal_test,Modal_test} from './component/SmartModal_test';
+ export { transformToFormField as apitransform } from './component/myDataGrid';
+ export { default as MyDropGrid ,  transformToFormField as apitransformDrop } from './component/myDropGrid';  
+export { default as Mytab } from './component/myTab';
+export { default as Loading } from './component/myload';   
+export { default as MyDropDown } from './component/myDropDown';  
+export { default as AppTitle } from './component/header';  
+export { default as Modal } from './component/myModal';  
 export { LoadingInline } from './component/myload'; 
-export { TextInput } from './component/simpleUI.tsx'; 
-export {  default as SmartModal } from './component/simpleUI.tsx'; 
- export { default as SmartModal_test,Modal_test} from './component/SmartModal_test'; 
-
-
+export { TextInput } from './component/simpleUI'; 
  
+
 
  
  
