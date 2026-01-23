8df432540b3896450dae0845629afcbb55a6e4ee
104259 <104259@RD-03250003>
2026-01-23 18:46:18 +0800

    build

--- src/index.js
+++ src/index.js
@@ -15,22 +15,20 @@
 //  export { default as SmartModal_test,Modal_test} from './component/SmartModal_test'; 
 
 
-export { default as Button ,Button2} from './component/button';
+export { default as Button, Button2 } from './component/button';
 export { default as DataGridApi } from './component/myDataGrid';// 這樣會同時匯出組件與型別
-export { default as DynamicForm} from './component/DynamicForm';
-export { default as SmartModal} from './component/SmartModal';
- export { default as SmartModal_test,Modal_test} from './component/SmartModal_test';
- export { transformToFormField as apitransform } from './component/myDataGrid';
- export { default as MyDropGrid ,  transformToFormField as apitransformDrop } from './component/myDropGrid';  
+export { default as DynamicForm } from './component/DynamicForm';
+export { default as SmartModal } from './component/SmartModal';
+export { default as SmartModal_test, Modal_test } from './component/SmartModal_test';
+export { transformToFormField as apitransform } from './component/myDataGrid';
+export { default as MyDropGrid, transformToFormField as apitransformDrop } from './component/myDropGrid';
 export { default as Mytab } from './component/myTab';
-export { default as Loading } from './component/myload';   
-export { default as MyDropDown } from './component/myDropDown';  
-export { default as AppTitle } from './component/header';  
-export { default as Modal } from './component/myModal';  
-export { LoadingInline } from './component/myload'; 
-export { TextInput } from './component/simpleUI'; 
+export { default as Loading } from './component/myload';
+export { default as MyDropDown } from './component/myDropDown';
+export { default as AppTitle } from './component/header';
+export { default as Modal } from './component/myModal';
+export { LoadingInline } from './component/myload';
+export { TextInput } from './component/simpleUI';
  
 
 
- 
- 
--- src/test.d.ts
+++ src/test.d.ts
@@ -1,24 +1,22 @@
-// export const Button: any;
-// export const Button2: any;
-//export const DataGridApi: any;
  
-export { default as Button ,Button2} from './component/button';
-export { default as DataGridApi } from './component/myDataGrid';// 這樣會同時匯出組件與型別
-export { default as DynamicForm} from './component/DynamicForm';
-export { default as SmartModal} from './component/SmartModal';
-//export { default as SmartModal2} from './component/SmartModal';
- export { default as SmartModal_test,Modal_test} from './component/SmartModal_test';
+ 
+// export { default as Button ,Button2} from './component/button';
+// export { default as DataGridApi } from './component/myDataGrid';// 這樣會同時匯出組件與型別
+// export { default as DynamicForm} from './component/DynamicForm';
+// export { default as SmartModal} from './component/SmartModal';
+ 
+//  export { default as SmartModal_test,Modal_test} from './component/SmartModal_test';
 
-export const apitransform: any;
-export const apitransformDrop: any;
-export const Mytab: any;
-export const Loading: any;
-//export const DynamicForm: any;
-export const MyDropDown: any;
-export const MyDropGrid: any;
-export const AppTitle: any;
-export const Modal: any;
-export const LoadingInline: any;
-export const TextInput: any;
-//export const SmartModal: any;
+// export const apitransform: any;
+// export const apitransformDrop: any;
+// export const Mytab: any;
+// export const Loading: any;
+ 
+// export const MyDropDown: any;
+// export const MyDropGrid: any;
+// export const AppTitle: any;
+// export const Modal: any;
+// export const LoadingInline: any;
+// export const TextInput: any;
+ 
  
