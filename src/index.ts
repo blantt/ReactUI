cdb91a97e97dee31a57f841f426fe46e659d337f
104259 <104259@RD-03250003>
2026-01-23 19:22:47 +0800

    ui

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
 
-export * from './component/button'
-export *  from './component/myDataGrid';
-export *  from './component/DynamicForm';
-export *  from './component/SmartModal';
-export *  from './component/SmartModal_test';
-export *  from './component/myDataGrid';
-export *  from './component/myTab';
-export *  from './component/myload';
-//export *  from './component/myDropDown';
-export *  from './component/header';
-export *  from './component/myModal';
-//export *  from './component/simpleUI';
+// export * from './component/button'
+// export *  from './component/myDataGrid';
+// export *  from './component/DynamicForm';
+// export *  from './component/SmartModal';
+// export *  from './component/SmartModal_test';
+// export *  from './component/myDataGrid';
+// export *  from './component/myTab';
+// export *  from './component/myload';
+// //export *  from './component/myDropDown';
+// export *  from './component/header';
+// export *  from './component/myModal';
+// //export *  from './component/simpleUI';
+
+
+export { default as Button, Button2 } from './component/button';
+export { default as DataGridApi } from './component/myDataGrid';// 這樣會同時匯出組件與型別
+export { default as DynamicForm } from './component/DynamicForm';
+export { default as SmartModal } from './component/SmartModal';
+export { default as SmartModal_test, Modal_test } from './component/SmartModal_test';
+export { transformToFormField as apitransform } from './component/myDataGrid';
+export { default as MyDropGrid, transformToFormField as apitransformDrop } from './component/myDropGrid';
+export { default as Mytab } from './component/myTab';
+export { default as Loading } from './component/myload';
+export { default as MyDropDown } from './component/myDropDown';
+export { default as AppTitle } from './component/header';
+export { default as Modal } from './component/myModal';
+export { LoadingInline } from './component/myload';
+export { TextInput } from './component/simpleUI';
+ 
